66d4e52fb38f0258015ec70cc0b57e4df643ffd3
Shinyai <schulz4455@gmail.com>
2023-11-20 12:05:28 +0100

    changed stepsoundscript added foodstepsound types

--- Assets/Code/Scripts/StepSoundScript.cs
+++ Assets/Code/Scripts/StepSoundScript.cs
@@ -15,6 +15,7 @@ public class StepSoundScript : MonoBehaviour
 
     private AudioSource stepPlayer;
     private string currentFloortype;
+    private int stepIndex = -1;
 
     private void Awake()
     {
@@ -26,30 +27,13 @@ public class StepSoundScript : MonoBehaviour
         if (Floortypes != null)
         {
             currentFloortype = Floortypes.GetTile(new Vector3Int(Mathf.FloorToInt(transform.position.x), Mathf.FloorToInt(transform.position.y), 0)).name;
-            if (currentFloortype.Equals("Floortypes_Stone"))
-            {
-                stepPlayer.volume = Stepsounds[0].volume * (1f + UnityEngine.Random.Range(-Stepsounds[0].volumeVariance / 2f, Stepsounds[0].volumeVariance / 2f));
-                stepPlayer.pitch = Stepsounds[0].pitch * (1f + UnityEngine.Random.Range(-Stepsounds[0].pitchVariance / 2f, Stepsounds[0].pitchVariance / 2f));
-                stepPlayer.PlayOneShot(Stepsounds[0].clip);
-            }
-            if (currentFloortype.Equals("Floortypes_Wood"))
-            {
-                stepPlayer.volume = Stepsounds[1].volume * (1f + UnityEngine.Random.Range(-Stepsounds[1].volumeVariance / 2f, Stepsounds[1].volumeVariance / 2f));
-                stepPlayer.pitch = Stepsounds[1].pitch * (1f + UnityEngine.Random.Range(-Stepsounds[1].pitchVariance / 2f, Stepsounds[1].pitchVariance / 2f));
-                stepPlayer.PlayOneShot(Stepsounds[1].clip);
-            }
-            if (currentFloortype.Equals("Floortypes_Sand"))
-            {
-                stepPlayer.volume = Stepsounds[2].volume * (1f + UnityEngine.Random.Range(-Stepsounds[2].volumeVariance / 2f, Stepsounds[2].volumeVariance / 2f));
-                stepPlayer.pitch = Stepsounds[2].pitch * (1f + UnityEngine.Random.Range(-Stepsounds[2].pitchVariance / 2f, Stepsounds[2].pitchVariance / 2f));
-                stepPlayer.PlayOneShot(Stepsounds[2].clip);
-            }
-            if (currentFloortype.Equals("Floortypes_Water"))
-            {
-                stepPlayer.volume = Stepsounds[3].volume * (1f + UnityEngine.Random.Range(-Stepsounds[3].volumeVariance / 2f, Stepsounds[3].volumeVariance / 2f));
-                stepPlayer.pitch = Stepsounds[3].pitch * (1f + UnityEngine.Random.Range(-Stepsounds[3].pitchVariance / 2f, Stepsounds[3].pitchVariance / 2f));
-                stepPlayer.PlayOneShot(Stepsounds[3].clip);
-            }
+            if (currentFloortype.Equals("Floortypes_Stone")){stepIndex = 0; }
+            if (currentFloortype.Equals("Floortypes_Wood")){stepIndex = 1;}
+            if (currentFloortype.Equals("Floortypes_Sand")){stepIndex = 2;}
+            if (currentFloortype.Equals("Floortypes_Water")){stepIndex = 3;}
+            stepPlayer.volume = Stepsounds[stepIndex].volume * (1f + UnityEngine.Random.Range(-Stepsounds[stepIndex].volumeVariance / 2f, Stepsounds[stepIndex].volumeVariance / 2f));
+            stepPlayer.pitch = Stepsounds[stepIndex].pitch * (1f + UnityEngine.Random.Range(-Stepsounds[stepIndex].pitchVariance / 2f, Stepsounds[stepIndex].pitchVariance / 2f));
+            stepPlayer.PlayOneShot(Stepsounds[stepIndex].clip);
         }
     }
 }
