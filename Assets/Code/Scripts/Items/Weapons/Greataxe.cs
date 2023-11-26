8487107d91a61211d6696a8d7bcab9b6683fb822
Shinyai <schulz4455@gmail.com>
2023-11-27 00:04:14 +0100

    added 3 new weapons: greataxe, axe, longsword

--- Assets/Code/Scripts/Items/Weapons/Greataxe.cs
+++ Assets/Code/Scripts/Items/Weapons/Greataxe.cs
@@ -1,18 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static Toolbox;
 
-public class Greataxe : MonoBehaviour
+public class Greataxe : Weapon
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 attackBoxSize = new Vector2(1.4f, 1.4f);
+    public Vector2 attackOffset = new Vector2(0.7f, 0.7f);
+    private float playerTransformFactor = 1f;
+
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 0.5f) * new Vector2(1f, 1f)), attackBoxSize * 0.5f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 0.5f) * new Vector2(-1f, 1f)), attackBoxSize * 0.5f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 0.5f) * new Vector2(1f, -1f)), attackBoxSize * 0.5f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 0.5f) * new Vector2(-1f, -1f)), attackBoxSize * 0.5f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset) * new Vector2(1f, 1f)), attackBoxSize);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset) * new Vector2(-1f, 1f)), attackBoxSize);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset) * new Vector2(1f, -1f)), attackBoxSize);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset) * new Vector2(-1f, -1f)), attackBoxSize);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 1.75f) * new Vector2(1f, 1f)), attackBoxSize * 1.75f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 1.75f) * new Vector2(-1f, 1f)), attackBoxSize * 1.75f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 1.75f) * new Vector2(1f, -1f)), attackBoxSize * 1.75f);
+        Gizmos.DrawWireCube(transform.position + (Vector3)((attackOffset * 1.75f) * new Vector2(-1f, -1f)), attackBoxSize * 1.75f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Attack()
     {
-        
+        var attackDirection = player.GetPlayerFacingDirection();
+        var attackPos = (Vector2)player.transform.position;
+
+        Vector2 _attackOffset;
+        switch (attackDirection)
+        {
+            case DiagonalDirection.UpRight:
+                _attackOffset = attackOffset * new Vector2(1f, 1f) * playerTransformFactor;
+                break;
+            case DiagonalDirection.UpLeft:
+                _attackOffset = attackOffset * new Vector2(-1f, 1f) * playerTransformFactor;
+                break;
+            case DiagonalDirection.DownLeft:
+                _attackOffset = attackOffset * new Vector2(-1f, -1f) * playerTransformFactor;
+                break;
+            default: // DiagonalDirection.DownRight
+                _attackOffset = attackOffset * new Vector2(1f, -1f) * playerTransformFactor;
+                break;
+        }
+
+        var boxCast = Physics2D.BoxCastAll(attackPos + _attackOffset, attackBoxSize * playerTransformFactor, 0f, Vector2.zero, 0f, LayerMask.GetMask("Enemy"));
+        boxCast.ToList().ForEach(hit => {
+            var enemyController = hit.transform.GetComponent<Transform>(); // Transform durch EnemyController swappen
+
+            if (enemyController != null)
+            {
+                // TODO enemyController.Damage(this.damage);
+                print("Hit: " + enemyController.name + " with " + this.Damage);
+            }
+        });
+
+        audioManager.Play(AttackSound.name);
+    }
+
+    public override void OnPlayerScaleChange(PlayerScalingInfo updatedScalingLevelInfo)
+    {
+        base.OnPlayerScaleChange(updatedScalingLevelInfo);
+
+        playerTransformFactor = updatedScalingLevelInfo.TransformScale;
     }
 }
