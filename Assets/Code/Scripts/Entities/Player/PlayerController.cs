d5cf7064d60e76bf0cff7a4cf6be7283c6fbae6f
Florian Zimmer <delfin.97@gmx.de>
2024-01-28 11:31:25 +0100

    * Split Player Idle State into 4 idle states for each orientation * Refactored Player Animator accordingly * Removed unused reference in PlayerController

--- Assets/Code/Scripts/Entities/Player/PlayerController.cs
+++ Assets/Code/Scripts/Entities/Player/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using Weapons;
 using static Toolbox;
 
 public class PlayerController : MonoBehaviour, IDamageable, IActivityToggle
